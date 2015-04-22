b7434d90193afbf39a3a30f2a0ebe66f85010dfd
talpalaru <talpalaru@users.noreply.github.com>
2015-04-22 23:08:07 +0200

    dded Eclipse prefs

---
+++ PolTool/src/main/java/de/mt/ligaimport/normalizer/entities/MatchEntity.java
@@ -0,0 +1,11 @@
+package de.mt.ligaimport.normalizer.entities;
+
+import java.util.Collection;
+
+public class MatchEntity {
+	private String leftTeam;
+	private String rightTeam;
+	private String date;
+	private Collection<PlaySetEntity> sets;
+
+}
